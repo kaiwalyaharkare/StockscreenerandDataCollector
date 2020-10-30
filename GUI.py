051481346d059c2a4effa3c0312aa708d4860084
kaiwalya harkare <66217282+kaiwalyaharkare@users.noreply.github.com>
2020-10-30 11:24:20 +0530

    Saving Functionality to save data as exel and csv minor design changes

--- GUI.py
+++ GUI.py
@@ -5,9 +5,7 @@ from datetime import datetime as dt
 root = Tk()
 root.title("KNH Stockscreener")
 root.iconbitmap()
-root.geometry('250x250')
-
-
+root.geometry('400x250')
 
 
 # intializingelemnets -----------------------------------------------------
@@ -20,27 +18,52 @@ StartDate = Entry(root,width=10)
 
 EndDateLable = Label(root, text="Enter End Date YYYY,MM,DD",padx=10,pady=10)
 EndDate = Entry(root,width=10)
-#  Buttonpress fuction
+
+        
 
 def Getdata() :
     Start = dt.strptime(StartDate.get(),"%Y,%m,%d")
     End = dt.strptime(EndDate.get(),"%Y,%m,%d")
-    chart(Stockcode.get(),start=Start,end=End)
+    chart(Stockcode.get(),start=Start,end=End,labley=Stockcode.get())
+   # Bar(Stockcode.get(),start=Start,end=End,labley=Stockcode.get())
+
+def savecsv():
+     Start = dt.strptime(StartDate.get(),"%Y,%m,%d")
+     End = dt.strptime(EndDate.get(),"%Y,%m,%d")
+     df = web.DataReader(Stockcode.get(),'yahoo',Start,End)
+     namefile = Stockcode.get()+"Price Data "+StartDate.get()+"  to   "+EndDate.get()
+     df.to_csv(namefile)
+
+def saveexel():
+     Start = dt.strptime(StartDate.get(),"%Y,%m,%d")
+     End = dt.strptime(EndDate.get(),"%Y,%m,%d")
+     df = web.DataReader(Stockcode.get(),'yahoo',Start,End)
+     namefile = Stockcode.get()+"Price Data From "+StartDate.get()+"  to  "+EndDate.get()
+     df.to_excel(namefile +'.xlsx',engine= "xlsxwriter")
 
 
-Submit = Button(root,text="SUBMIT",command=Getdata)
+
+Submit = Button(root,text="PLOT PRICE GRAPH ",command=Getdata,fg="green",padx=10,pady=10,height=1)
+Submitsavecsv = Button(root,text="SAVE ALL DATA  CSV",command=savecsv,fg="green",padx=10,pady=10,height=1)
+Submitsaveexel = Button(root,text="SAVE ALL DATA EXCEL",command=saveexel,fg="green",padx=10,pady=10,height=1)
+
+
 
 # Postioning the elements
-StockcodeLabel.pack()
-Stockcode.pack()
 
-StartDateLable.pack()
-StartDate.pack()
+StockcodeLabel.grid(column=1)
+Stockcode.grid(column=1)
 
-EndDateLable.pack()
-EndDate.pack()
+StartDateLable.grid(column=1)
+StartDate.grid(column=1)
 
-Submit.pack()
+EndDateLable.grid(column=1)
+EndDate.grid(column=1)
+
+Submit.grid(column=2,row=1)
+Submitsavecsv.grid(column=2,row=2)
+Submitsaveexel.grid(column=2,row=3)
+
 
 
 root.mainloop()
--- stockscreener.py
+++ stockscreener.py
@@ -1,14 +1,26 @@
-import datetime as dt
+from datetime import datetime as  dt
+from tkinter.font import names
+import matplotlib
 import matplotlib.pyplot as plt
 from   matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
-def chart(ticker,start,end):   
+matplotlib.style.use('seaborn-darkgrid')
+
+def chart(ticker,start,end,labley):   
     df = web.DataReader(ticker,'yahoo',start,end)
+    plt.title( labley +  " Chart ")
+    plt.ylabel("Price  "+ labley)
     plt.plot(df['Adj Close'])
     plt.show()
-
+def Bar(ticker,start,end,labley):   
+    df = web.DataReader(ticker,'yahoo',start,end)
+    plt.title( labley +  " Chart ")
+    plt.ylabel("Price"+ labley)
+    plt.bar(df['Adj Close'])
+    plt.bar()
+    plt.show()
 
         
 
